5f2f5609741cf9e2b141aa5fb334d65fb6257863
chanchalvashistha <chanchal.vashistha@ipragmatech.com>
2016-03-17 13:14:16 +0530

    Css and php changes for compatibility with every advance pods bulk action plugin
    Css changes for fields listing ui and make compatible with pods bulk
    action plugin

--- pods-export-csv.php
+++ pods-export-csv.php
@@ -25,11 +25,11 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-if (!class_exists('Pods_Export_Bulk_Actions')) {
+if (!class_exists('Pods_Export_Csv')) {
 
-	class Pods_Export_Bulk_Actions {
-		public $delimiter;
-		public $type;
+	class Pods_Export_Csv {
+		private $delimiter = ',';
+		private $type = 'sv';
 		
 		public function __construct() {
 			if(is_admin()) {
@@ -37,14 +37,12 @@ if (!class_exists('Pods_Export_Bulk_Actions')) {
 				add_filter( 'pods_ui_pre_init', array(&$this, 'pods_ui_add_bulk_actions') , 10, 3 );
 				add_action( 'admin_enqueue_scripts', array(&$this, 'pods_ui_admin_enqueue_scripts'));
 			}
-			
-			$this->delimiter = ',';
-			$this->type = 'sv';
 		}
 		
 		public function pods_ui_admin_enqueue_scripts(){
 			if( stripos(get_current_screen()->id,'pods-manage') ) {
 				wp_register_style( 'pods-export-csv', plugin_dir_url( __FILE__ ) . 'css/pods-export-csv.css', false, '1.0.0' );
+				wp_enqueue_style( 'pods-export-csv' );
 			}
 		}
 		
@@ -71,10 +69,9 @@ if (!class_exists('Pods_Export_Bulk_Actions')) {
 			$page = explode("-",$_GET['page']);
 			$this->pod = pods($page[2]);
 			
-			if (isset($_POST['export_company'])) {
+			if (isset($_REQUEST['export_company'])) {
 				$export_fields = empty ( $_POST ['export_fields'] ) ? array () : $_POST ['export_fields'];
-				error_log(print_r($export_fields, true));
-				
+
 				foreach ( $this->pod->fields() as $field ) {
 					if (in_array ( $field ['id'], $export_fields )) {
 						$columns [$field ['name']] = $field ['label'];
@@ -85,11 +82,11 @@ if (!class_exists('Pods_Export_Bulk_Actions')) {
 				
 				$params = array(
 						'where' => "id IN ($ids)",
-						'orderby' => "id desc",
+						'orderby' => "id asc",
 						'limit' => -1
 				);
 					
-				$this->pod = pods ( 'small_company',$params );
+				$this->pod = pods ( $page[2],$params );
 				
 				$data = array (
 						'columns' => $columns,
@@ -97,15 +94,15 @@ if (!class_exists('Pods_Export_Bulk_Actions')) {
 						'fields' => $this->pod->fields()
 				);
 				
-				error_log(print_r($this->type, true));
-				error_log(print_r($this->delimiter, true));
-					
 				$migrate = pods_migrate ( $this->type, $this->delimiter, $data );
 					
 				$migrate->export ();
 					
 				$export_file = $migrate->save ();
-				error_log(print_r($export_file, true));
+
+				if ($export_file){
+					printf ( __ ( '<div class="updated notice"><p><strong>Success:</strong> Your export is ready, you can download it <a href="%s" target="_blank">here</a></p></div>', 'pods' ), $export_file );
+				}
 			}
 			
 			$this->fields_form();
@@ -118,16 +115,15 @@ if (!class_exists('Pods_Export_Bulk_Actions')) {
 			?>
 			<div class="wrap pods-admin pods-ui">
 				<h2>Choose Export Fields</h2>
-				<?php if ( !empty( $export_file ) ){ ?>
-					<div id="message" class="updated notice">
-						<p>
-							<strong>Success:</strong> Your export is ready, you can download it 
-							<a href="<?php echo $export_file;?>" target="_blank">here</a>
-						</p>
-					</div>
-								
-				<?php }?>
 				<form method="POST" id="export_form" class="ac-custom ac-checkbox ac-cross">
+				<?php  foreach ($_GET as $key => $value){ 
+						if( $key== "action_bulk_ids" ){
+				?>
+						<input type="hidden" name= "<?php echo $key;?>[]" value="<?php echo implode(",",$value);?>">
+						<?php } else{ ?>
+							<input type="hidden" name= "<?php echo $key;?>" value="<?php echo $value?>">
+						<?php } ?>
+				<?php } ?>
 					<ul>
 						<?php foreach (  $this->pod->fields() as $field_name => $detail ) { ?>
 							<li class="av_one_fourth">
@@ -144,4 +140,4 @@ if (!class_exists('Pods_Export_Bulk_Actions')) {
 	}
 }
 
-$Pods_Export_Bulk_Actions = new Pods_Export_Bulk_Actions ();
+$Pods_Export_Csv = new Pods_Export_Csv ();
